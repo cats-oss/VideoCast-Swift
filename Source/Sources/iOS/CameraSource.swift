9f16f9068cee32c7f44643a5a110f09bd3c0610d
Tomohiro Matsuzawa <thmatuza75@hotmail.com>
2018-09-09 23:14:51 +0900

    syncSafe utility function

--- Source/Sources/iOS/CameraSource.swift
+++ Source/Sources/iOS/CameraSource.swift
@@ -275,7 +275,7 @@ open class CameraSource: ISource {
                     ret = false
                 }
             } else {
-                Logger.error("Torch not available in current camera input")
+                Logger.info("Torch not available in current camera input")
             }
 
         }
@@ -423,7 +423,7 @@ open class CameraSource: ISource {
 
         var orientation: UIInterfaceOrientation = .unknown
 
-        DispatchQueue.main.sync {
+        syncSafe {
             if useInterfaceOrientation {
                 orientation = UIApplication.shared.statusBarOrientation
             } else {
